61c934a046be2245636a3748490cce90cf2050cc
Josh Ford <thisisjoshford@gmail.com>
2020-02-13 21:35:44 -0800

    created dynamic search box

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
 import PokeList from './PokeList.js'
 import Header from './Header.js'
+import './App.css'
 import request from 'superagent';
 
 export default class App extends Component {
 
   state = {
-    pokemon: []
+    pokemon: [],
+    search: ""
   }
   componentDidMount = async() => {
-   const data = await request.get('https://alchemy-pokedex.herokuapp.com/api/pokedex')
+   const data = await request.get('https://alchemy-pokedex.herokuapp.com/api/pokedex?page=3&perPage=50')
    
-   this.setState({pokemon: data.body.results})
+  this.setState({pokemon: data.body.results})
    console.log(this.state.pokemon)
+  }
+
+  updateSearch(event) {
+    //updates the search state based on user input and limits entry to 20 char
+    this.setState({search: event.target.value.substr(0,20)})
+    console.log(this.state.search)
   }
   render(){
    return (
      <div>
        <Header/>
+       <div className="searchBar">
+         <input placeholder="      search..." value={this.state.search} onChange={this.updateSearch.bind(this)}/>
+       </div>
        <PokeList pokemonz={this.state.pokemon}/>
      </div>
    )
--- src/PokeItem.js
+++ src/PokeItem.js
@@ -11,7 +11,7 @@ export default class PokeItem extends Component {
             </div>
             <div className="labelBox">
                 <h3>{obj.pokemon.toUpperCase()}</h3>
-                <p>Abillity: {obj.ability_1}</p>
+                <p>Ability: {obj.ability_1}</p>
                 <a href={obj.pokedex}>Info</a>
             </div>
         </li>
