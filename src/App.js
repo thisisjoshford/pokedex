444195cee6522b4ee20403712f433eae3e365126
Josh Ford <thisisjoshford@gmail.com>
2020-02-15 10:06:49 -0800

    created renderPoke and getPokemon function

--- src/App.js
+++ src/App.js
@@ -1,35 +1,58 @@
 import React, { Component } from 'react';
 import PokeList from './PokeList.js'
+import { getPokemon } from './renderPoke.js'
 import Header from './Header.js'
 import './App.css'
 import request from 'superagent';
+import { wait } from '@testing-library/react';
 
 export default class App extends Component {
-
+  //create empty containers (variables) in state
   state = {
     pokemon: [],
     search: ""
   }
-  componentDidMount = async() => {
-   const data = await request.get('https://alchemy-pokedex.herokuapp.com/api/pokedex?page=3&perPage=50')
-   
-  this.setState({pokemon: data.body.results})
-   console.log(this.state.pokemon)
+  //getting pokemon from api using getPokemon function... we use async/await because we are working with an api that has delay in returning results
+  async loadPokemon() {
+    //returns array of objects (count, page, perPage, sort, search, results) results contain array of pokemon objects
+    const apiResponse = await getPokemon();
+    console.log(apiResponse)
+    //place the array of pokemon objects into pokemon variable
+    const pokemon = apiResponse.results;
+    //place the total count 
+    const numberOfPokemon = apiResponse.count;
+    //set state with above data
+    this.setState({
+      pokemon: pokemon,
+      numberOfPokemon: numberOfPokemon
+    }
+    )
   }
 
+  componentDidMount = async() => {
+    await this.loadPokemon();
+      window.addEventListener('hashchange', async() => {
+      await this.loadPokemon();
+    })
+  }
+
+  //updates the search state based on user input and limits entry to 20 char
   updateSearch(event) {
-    //updates the search state based on user input and limits entry to 20 char
     this.setState({search: event.target.value.substr(0,20)})
     console.log(this.state.search)
   }
+  
   render(){
+
+  const { numberOfPokemon, pokemon } = this.state;
+
    return (
      <div>
        <Header/>
        <div className="searchBar">
-         <input placeholder="      search..." value={this.state.search} onChange={this.updateSearch.bind(this)}/>
+         <input placeholder="      search..." value={this.state.search} onChange={this.updateSearch.bind(this)}/>🔍
        </div>
-       <PokeList pokemonz={this.state.pokemon}/>
+       <PokeList pokemonz={pokemon}/>
      </div>
    )
  }
